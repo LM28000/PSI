ce3fe44e11803ff86898138970186839e98f12b1
Louis-Marie PERRET DU CRAY <louis-marie@du-cray.com>
2025-01-23 10:19:28 +0100

    rmsufn

--- PSI/Program.cs
+++ PSI/Program.cs
@@ -4,7 +4,28 @@
     {
         static void Main(string[] args)
         {
+            string filepath = "soc-karate.mtx";
+            //Lire le fichier avec ReadLines
+            string[] lines = File.ReadAllLines(filepath);
+            //Créer liens
+            List<Lien> liens = new List<Lien>();
+            for (int i = 0; i < lines.Length; i++)
+            {
+                string[] noeuds = lines[i].Split(' ');
+                Noeud noeud1 = new Noeud(int.Parse(noeuds[0]));
+                Noeud noeud2 = new Noeud(int.Parse(noeuds[1]));
+                liens.Add(new Lien(noeud1, noeud2));
+            }
+            //Afficher les liens
+            foreach (Lien lien in liens)
+            {
+                Console.WriteLine("Lien entre noeud " + lien.noeud1.id + " et noeud " + lien.noeud2.id);
+            }
             
+
+
+
+
         }
     }
 }
