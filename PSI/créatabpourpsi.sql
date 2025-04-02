ad822f455d139badc1be2a92f3ef4a2a853f4d18
Léo Rabiat-humbert <leo.rabiat--humbert@edu.devinci.fr>
2025-04-02 17:08:53 +0200

    files sql
    ce sont les files pour la base de données de notre projet psi

---
+++ PSI/créatabpourpsi.sql
@@ -0,0 +1,89 @@
+DROP DATABASE IF EXISTS projet;
+CREATE DATABASE projet;
+USE projet;
+
+CREATE TABLE Entreprise_locale(
+   ID_entreprise VARCHAR(50),
+   Nom_entreprise VARCHAR(50),
+   Nome_referent VARCHAR(50),
+   PRIMARY KEY(ID_entreprise)
+);
+
+CREATE TABLE Commande(
+   ID_commande VARCHAR(50),
+   Nom VARCHAR(50),
+   Prix INT,
+   Quantite INT,
+   PRIMARY KEY(ID_commande)
+);
+
+CREATE TABLE Plat(
+   ID_plat VARCHAR(50),
+   Plat VARCHAR(50),
+   Date_de_fabrication DATE,
+   Date_de_peremption DATE,
+   Regime VARCHAR(50),
+   Nature VARCHAR(50),
+   Photo VARCHAR(50),
+   ID_commande VARCHAR(50) NOT NULL,
+   PRIMARY KEY(ID_plat),
+   FOREIGN KEY(ID_commande) REFERENCES Commande(ID_commande)
+);
+
+CREATE TABLE Ingredient(
+   ID_ingredient VARCHAR(50),
+   Nom VARCHAR(50),
+   Volume INT,
+   PRIMARY KEY(ID_ingredient)
+);
+
+CREATE TABLE Cuisinier(
+   ID_cuisinier VARCHAR(50),
+   ID_commande VARCHAR(50) NOT NULL,
+   PRIMARY KEY(ID_cuisinier),
+   FOREIGN KEY(ID_commande) REFERENCES Commande(ID_commande)
+);
+
+CREATE TABLE Particulier(
+   ID_Particulier VARCHAR(50),
+   Prenom VARCHAR(50),
+   Nom VARCHAR(50),
+   Rue VARCHAR(50),
+   Numero_rue INT,
+   Ville VARCHAR(50),
+   Code_postal INT,
+   Telephone INT,
+   Email VARCHAR(50),
+   metro__plus_proche VARCHAR(50),
+   ID_cuisinier VARCHAR(50) NOT NULL,
+   PRIMARY KEY(ID_Particulier),
+   UNIQUE(ID_cuisinier),
+   FOREIGN KEY(ID_cuisinier) REFERENCES Cuisinier(ID_cuisinier)
+);
+
+CREATE TABLE Client(
+   ID_client VARCHAR(50),
+   ID_Particulier VARCHAR(50),
+   ID_entreprise VARCHAR(50),
+   PRIMARY KEY(ID_client),
+   UNIQUE(ID_Particulier),
+   UNIQUE(ID_entreprise),
+   FOREIGN KEY(ID_Particulier) REFERENCES Particulier(ID_Particulier),
+   FOREIGN KEY(ID_entreprise) REFERENCES Entreprise_locale(ID_entreprise)
+);
+
+CREATE TABLE Est_compose(
+   ID_plat VARCHAR(50),
+   ID_ingredient VARCHAR(50),
+   PRIMARY KEY(ID_plat, ID_ingredient),
+   FOREIGN KEY(ID_plat) REFERENCES Plat(ID_plat),
+   FOREIGN KEY(ID_ingredient) REFERENCES Ingredient(ID_ingredient)
+);
+
+CREATE TABLE Passe_commande(
+   ID_client VARCHAR(50),
+   ID_commande VARCHAR(50),
+   PRIMARY KEY(ID_client, ID_commande),
+   FOREIGN KEY(ID_client) REFERENCES Client(ID_client),
+   FOREIGN KEY(ID_commande) REFERENCES Commande(ID_commande)
+);
